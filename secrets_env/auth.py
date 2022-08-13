8d5cc9f669fcf6151450fdb9e5b56064a4019b39
Tim Shih <tshih@netbasequid.com>
2022-08-13 23:53:20 +0800

    add prompt

--- secrets_env/auth.py
+++ secrets_env/auth.py
@@ -1,15 +1,18 @@
 import abc
+import importlib
 import logging
 import os
 import pathlib
+import sys
 import typing
 from dataclasses import dataclass, field
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Union
 
 import keyring
 import keyring.errors
 
 if typing.TYPE_CHECKING:
+    import click
     import hvac
 
 
@@ -17,9 +20,59 @@ logger = logging.getLogger(__name__)
 
 
 def get_password(name: str) -> Optional[str]:
+    """Wrapped `keyring.get_password`. Do not raise error when there is no
+    keyring backend enabled."""
     try:
         return keyring.get_password("secrets.env", name)
     except keyring.errors.NoKeyringError:
+        return None
+
+
+def prompt(
+    text: str,
+    default: Optional[Any] = None,
+    hide_input: bool = False,
+    type: Optional[Union["click.types.ParamType", Any]] = None,
+    show_default: bool = True,
+) -> Optional[Any]:
+    """Wrapped `click.prompt` function. Only shows the prompt when click is
+    installed and this feature is not disabled.
+
+    Parameters
+    ----------
+    text : str
+        The text to show for the prompt.
+    default : Optional[Any]
+        The default value to use if no input happens. If this is not given it
+        will prompt until it's aborted.
+    hide_input : bool
+        If this is set to true then the input value will be hidden.
+    type : Optional[Union[click.types.ParamType, Any]]
+        The type to use to check the value against.
+    show_default : bool
+        Shows or hides the default value in the prompt.
+    """
+    # skip prompt if click is not installed
+    try:
+        click = importlib.import_module("click")
+    except ImportError:
+        return None
+
+    # skip prompt if the env var is set
+    env = os.getenv("SECRETS_ENV_NO_PROMPT", "FALSE")
+    if env.upper() in ("TRUE", "T", "YES", "Y", "1"):
+        return None
+
+    try:
+        return click.prompt(
+            text=text,
+            default=default,
+            hide_input=hide_input,
+            type=type,
+            show_default=show_default,
+        )
+    except click.Abort:
+        sys.stdout.write(os.linesep)
         return None
 
 
@@ -120,20 +173,24 @@ class UserPasswordAuth(Auth):
             username = data.get("username")
         if not username:
             username = get_password(f"{method}/:username")
+        if not username:
+            username = prompt(f"Username for {method} auth")
         if not isinstance(username, str):
             logger.error(
-                "Missing auth information: username. Neither key 'username' in "
-                "config nor environment variable `VAULT_USERNAME` is found."
+                "Missing username for %s auth. Stop loading secrets.",
+                method,
             )
             return None
 
         password = os.getenv("VAULT_PASSWORD")
         if not password:
             password = get_password(f"{method}/{username}")
+        if not password:
+            password = prompt("Password", hide_input=True)
         if not isinstance(password, str):
             logger.error(
-                "Missing auth information: password. "
-                "Environment variable `VAULT_PASSWORD` not found."
+                "Missing password for %s auth. Stop loading secrets.",
+                method,
             )
             return None
 
