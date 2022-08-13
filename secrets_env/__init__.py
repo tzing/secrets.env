68e6c64d4b4c3d6d80aaad1f2da3eeb661e203c5
Tim Shih <tshih@netbasequid.com>
2022-08-14 00:45:48 +0800

    [ci skip] bump version

--- secrets_env/__init__.py
+++ secrets_env/__init__.py
@@ -6,7 +6,7 @@ __all__ = [
     "load_config",
 ]
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 
 from secrets_env.auth import OktaAuth, TokenAuth
 from secrets_env.config import find_config, load_config
