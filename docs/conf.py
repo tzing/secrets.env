b0bde25b9e6fd9c307f9f805f991e6f66ab29621
tzing <tzingshih@gmail.com>
2022-09-21 21:31:20 +0800

    add makefile and conf

--- docs/conf.py
+++ docs/conf.py
@@ -3,25 +3,48 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import datetime
+import pathlib
+import sys
 
-project = 'secrets.env'
-copyright = '2022, tzing'
-author = 'tzing'
+docdir = pathlib.Path(__file__).resolve().parent
+repodir = docdir.parent
+sys.path.insert(0, str(repodir))
+
+import secrets_env
+
+
+# -- Project information -----------------------------------------------------
+
+this_year = datetime.date.today().year
+
+project = "secrets.env"
+copyright = f"{this_year}, tzing"
+author = "tzing"
+version = secrets_env.__version__
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+
+html_theme_options = {
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/tzing/secrets.env",
+            "icon": "fab fa-github-square",
+        }
+    ],
+    "show_toc_level": 1,
+}
