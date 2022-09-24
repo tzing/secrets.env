f22282e7c3884d285d8bea870661b39cff64e68f
tzing <tzingshih@gmail.com>
2022-09-24 18:51:58 +0800

    add: load config

--- secrets_env/config/__init__.py
+++ secrets_env/config/__init__.py
@@ -1,142 +1,38 @@
 import logging
-import os
-import re
-from pathlib import Path
-from typing import Any, Optional, Tuple, Union
+import typing
+from typing import Optional
 
-import secrets_env.auth
+from secrets_env.config.file import (
+    build_config_file_metadata,
+    find_config_file,
+    read_config_file,
+)
+from secrets_env.config.parse import parse_config
 
-from .file import build_config_file_metadata, find_config_file, read_config_file
-from .parse import parse_section_secrets
-from .types import Config
-from .types import ConfigFileMetadata as ConfigFile
-from .types import SecretPath as SecretResource
+if typing.TYPE_CHECKING:
+    from pathlib import Path
+
+    from secrets_env.config.types import Config
 
 logger = logging.getLogger(__name__)
 
 
-def find_config(*args, **kwargs):
-    pass
-
-
-def load_config(path: Optional[Path] = None) -> Optional[Config]:
-    """Load the configurations and formated in to the typed structure. Values
-    are loaded NOT ONLY from the config file, it could be:
-      1. environment variable
-      2. config file
-      3. system keyring service
-      4. prompt
-    When a value has more than one occurrence, the first occurrence would be
-    selected based on the order above.
-    """
-    # find config file
+def load_config(path: Optional["Path"] = None) -> Optional["Config"]:
+    """Load the configurations and formated in to the typed structure."""
     if path:
         file_metadata = build_config_file_metadata(path)
     else:
         file_metadata = find_config_file()
 
     if not file_metadata:
-        logger.debug("Config file not found.")
+        logger.info("Config file not found.")
         return None
 
     logger.info("Read secrets.env config from <data>%s</data>", file_metadata.path)
 
-    # read it
     data = read_config_file(file_metadata)
     if not data:
-        logger.debug("Configure section not found. Stop loading secrets.")
+        logger.info("No content in the config file. Stop loading secrets.")
         return None
 
-    # parse
-    config, ok = _loads(data)
-    if not ok:
-        return None
-
-    return config
-
-
-def _loads(data: dict) -> Tuple[Config, bool]:  # noqa: CCR001
-    """Loads config from various sources and structure them into the Config
-    object.
-
-    This function tries to parse every thing instead of raise the error
-    immediately. This behavior is preserved to expose every potential errors to
-    users.
-    """
-    ok = True
-
-    def assert_type(name: str, expect: str, obj: Any):
-        nonlocal ok
-        __defaults = {
-            "str": (str, None),
-            "dict": (dict, {}),
-        }
-        __max_len = 20
-
-        # check type
-        type_, default_value = __defaults[expect]
-        if isinstance(obj, type_):
-            return obj
-
-        # print error message
-        preview = str(obj)
-        if len(preview) > __max_len:
-            preview = preview[: __max_len - 3] + "..."
-
-        logger.error(
-            f"Config malformed: <data>{name}</data>. "
-            f"Expected <mark>{expect}</mark> type, "
-            f"got '<data>{preview}</data>' (<mark>{type(obj).__name__}</mark> type)."
-        )
-
-        ok = False
-        return default_value
-
-    # 'source' section - address and auth
-    data_source = data.get("source", {})
-    data_source = assert_type("source", "dict", data_source)
-
-    # url
-    url = os.getenv("SECRETS_ENV_ADDR")
-    if not url:
-        url = os.getenv("VAULT_ADDR")
-    if not url:
-        url = data_source.get("url", None)
-
-    if url:
-        url = assert_type("source.url", "str", url)
-    else:
-        logger.error(
-            "Missing required config: <data>url</data>. Neither the value "
-            "'<mark>source.url</mark>' in the config file nor the environment "
-            "variable '<mark>SECRETS_ENV_ADDR</mark>' is found."
-        )
-        ok = False
-
-    # auth method
-    data_auth = data_source.get("auth", {})
-    if isinstance(data_auth, str):
-        # allow `auth: token` syntax in config
-        data_auth = {
-            "method": data_auth,
-        }
-
-    data_auth = assert_type("auth", "dict", data_auth)
-
-    auth = secrets_env.auth.load_auth(data_auth)
-    if not auth:
-        ok = False
-
-    # 'secrets' section
-    data_secrets = data.get("secrets", {})
-    if data_secrets:
-        data_secrets = assert_type("secrets", "dict", data_secrets)
-    else:
-        logger.warning(
-            "'<mark>secrets</mark>' section is empty. No data would be loaded."
-        )
-        data_secrets = {}
-
-    secrets = parse_secrets_section(data_secrets)
-
-    return Config(url=url, auth=auth, secret_specs=secrets), ok
+    return parse_config(data)
---
+++ tests/config/test___init__.py
@@ -0,0 +1,90 @@
+import logging
+from pathlib import Path
+
+import pytest
+
+import secrets_env.config as t
+from secrets_env.auth import TokenAuth
+from secrets_env.config.types import Config, ConfigFileMetadata, SecretPath
+
+
+class TestLoadConfig:
+    @pytest.mark.parametrize(
+        ("filename", "spec"),
+        [
+            (".secrets-env.json", "json"),
+            (".secrets-env.toml", "toml"),
+            (".secrets-env.yaml", "yaml"),
+            ("pyproject.toml", "pyproject.toml"),
+        ],
+    )
+    def test_success_1(
+        self,
+        monkeypatch: pytest.MonkeyPatch,
+        repo_path: Path,
+        filename: str,
+        spec: str,
+    ):
+        # fixtures
+        def find_config_file():
+            return ConfigFileMetadata(
+                filename, spec, True, repo_path / "example" / filename
+            )
+
+        monkeypatch.setattr(t, "find_config_file", find_config_file)
+        monkeypatch.setenv("SECRETS_ENV_TOKEN", "ex@mp1e")
+
+        # run
+        cfg = t.load_config()
+
+        # test
+        assert isinstance(cfg, Config)
+        assert cfg.url == "https://example.com/"
+        assert cfg.auth == TokenAuth("ex@mp1e")
+        assert cfg.secret_specs == {
+            "VAR1": SecretPath("kv/default", "example"),
+            "VAR2": SecretPath("kv/default", "example"),
+        }
+
+    @pytest.mark.parametrize(
+        "filename",
+        [
+            ".secrets-env.json",
+            ".secrets-env.toml",
+            ".secrets-env.yaml",
+            "pyproject.toml",
+        ],
+    )
+    def test_success_2(
+        self, monkeypatch: pytest.MonkeyPatch, repo_path: Path, filename: str
+    ):
+
+        monkeypatch.setenv("SECRETS_ENV_TOKEN", "ex@mp1e")
+
+        path = repo_path / "example" / filename
+        cfg = t.load_config(path)
+
+        assert isinstance(cfg, Config)
+        assert cfg.url == "https://example.com/"
+        assert cfg.auth == TokenAuth("ex@mp1e")
+        assert cfg.secret_specs == {
+            "VAR1": SecretPath("kv/default", "example"),
+            "VAR2": SecretPath("kv/default", "example"),
+        }
+
+    def test_not_found(self, monkeypatch: pytest.MonkeyPatch):
+        # find file
+        monkeypatch.setattr(t, "find_config_file", lambda: None)
+        assert t.load_config() is None
+
+        # use file
+        assert t.load_config(Path("/no-this-data.json")) is None
+
+    def test_not_content(self, caplog: pytest.LogCaptureFixture, tmp_path: Path):
+        path = tmp_path / "empty.json"
+
+        with caplog.at_level(logging.INFO):
+            assert t.load_config(path) is None
+
+        assert "Read secrets.env config from " in caplog.text
+        assert "No content in the config file." in caplog.text
