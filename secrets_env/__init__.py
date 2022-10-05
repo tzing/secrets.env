e35bf79da930059f3fffb9745ca2075c710d2d74
tzing <tzingshih@gmail.com>
2022-10-05 23:10:23 +0800

    add load_secrets

--- secrets_env/__init__.py
+++ secrets_env/__init__.py
@@ -1,12 +1,43 @@
-__all__ = [
-    "KVReader",
-    "OktaAuth",
-    "TokenAuth",
-    "load_config",
-]
-
+__name__ = "secrets.env"
 __version__ = "0.8.0"
 
-from secrets_env.auth import OktaAuth, TokenAuth
-from secrets_env.config import load_config
-from secrets_env.reader import KVReader
+import logging
+from pathlib import Path
+from typing import Dict
+
+import secrets_env.config
+import secrets_env.reader
+
+logger = logging.getLogger(__name__)
+
+
+def load_secrets(config_file: Path = None) -> Dict[str, str]:
+    """Load secrets from vault and put them to environment variable."""
+    config = secrets_env.config.load_config(config_file)
+    if not config:
+        # skip logging. already show error in `load_config`
+        return {}
+
+    reader = secrets_env.reader.KVReader(config.url, config.auth)
+    secrets = reader.get_values(config.secret_specs.values())
+
+    output = {}
+    for name, spec in config.secret_specs.items():
+        value = secrets.get(spec)
+        if not value:
+            # skip logging. already show warning in `get_value`
+            continue
+
+        logger.debug("Load <info>%s</info>", name)
+        output[name] = value
+
+    if len(output) == len(config.secret_specs):
+        logger.info("<info>%d</info> secrets loaded", len(secrets))
+    else:
+        logger.warning(
+            "<error>%d</error> / %d secrets loaded",
+            len(output),
+            len(config.secret_specs),
+        )
+
+    return output
---
+++ tests/test___init__.py
@@ -0,0 +1,59 @@
+import logging
+
+from unittest.mock import Mock, patch
+
+import pytest
+
+import secrets_env
+from secrets_env.config.types import Config, SecretPath
+
+
+class TestLoadSecrets:
+    @pytest.fixture()
+    def patch_load_config(self):
+        with patch(
+            "secrets_env.config.load_config",
+            return_value=Config(
+                url="https://example.com/",
+                auth=secrets_env.auth.TokenAuth("ex@mp1e"),
+                tls={},
+                secret_specs={
+                    "VAR1": SecretPath("key1", "example"),
+                    "VAR2": SecretPath("key2", "example"),
+                },
+            ),
+        ) as mock:
+            yield mock
+
+    @pytest.mark.usefixtures("patch_load_config")
+    def test_success(self, caplog: pytest.LogCaptureFixture):
+        with patch(
+            "secrets_env.reader.KVReader.get_values",
+            return_value={
+                SecretPath("key1", "example"): "foo",
+                SecretPath("key2", "example"): "bar",
+            },
+        ), caplog.at_level(logging.INFO):
+            assert secrets_env.load_secrets(None) == {
+                "VAR1": "foo",
+                "VAR2": "bar",
+            }
+
+        assert "<info>2</info> secrets loaded" in caplog.text
+
+    @pytest.mark.usefixtures("patch_load_config")
+    def test_partial_loaded(self, caplog: pytest.LogCaptureFixture):
+        with patch(
+            "secrets_env.reader.KVReader.get_values",
+            return_value={
+                # no key2
+                SecretPath("key1", "example"): "foo",
+            },
+        ):
+            assert secrets_env.load_secrets(None) == {"VAR1": "foo"}
+
+        assert "<error>1</error> / 2 secrets loaded" in caplog.text
+
+    def test_no_config(self, patch_load_config: Mock):
+        patch_load_config.return_value = None
+        assert secrets_env.load_secrets(None) == {}
