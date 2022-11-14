d61f2566a5cee5a1f6413cdb5e2673a47c9bd130
tzing <tzingshih@gmail.com>
2022-11-14 14:16:44 +0800

    add test

--- tests/config/test_file.py
+++ tests/config/test_file.py
@@ -8,6 +8,48 @@ import pytest
 import secrets_env.config.file as t
 
 
+@pytest.fixture()
+def fixture_dir(repo_path: Path) -> Path:
+    return repo_path / "tests" / "fixtures"
+
+
+class TestConfigFile:
+    @pytest.mark.parametrize(
+        ("filename", "format_"),
+        [
+            (".secrets-env.json", "json"),
+            (".secrets-env.yaml", "yaml"),
+            (".secrets-env.yml", "yaml"),
+            (".secrets-env.toml", "toml"),
+            ("pyproject.toml", "pyproject.toml"),
+        ],
+    )
+    def test_success(
+        self,
+        monkeypatch: pytest.MonkeyPatch,
+        tmp_path: Path,
+        filename: str,
+        format_: str,
+    ):
+        monkeypatch.chdir(tmp_path)
+
+        # create fake config file
+        (tmp_path / filename).touch()
+
+        # run test
+        spec = t.find_config_file()
+        assert spec.filename == filename
+        assert spec.format == format_
+        assert spec.path == tmp_path / filename
+
+    def test_multiple(self, fixture_dir: Path):
+        # TOML is top prioritized and we must have toml parser installed in testing env
+        spec = t.find_config_file(fixture_dir)
+        assert spec.filename == ".secrets-env.toml"
+        assert spec.format == "toml"
+        assert spec.path == fixture_dir / ".secrets-env.toml"
+
+
 def test_check_installed():
     assert t.check_installed("json") is True
     assert t.check_installed("module-not-exists", "json") is True
