0915410b89796bd719a8947fd9edc2d991b939c0
tzing <tzingshih@gmail.com>
2022-11-16 20:14:36 +0800

    extend scope

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,18 +8,23 @@ import respx
 
 
 @pytest.fixture()
-def repo_path() -> Path:
-    this_dir = Path(__file__).resolve().parent
-    return this_dir.parent
-
-
-@pytest.fixture()
 def _reset_logging():
     yield
     for logger in (logging.root, logging.getLogger("secrets_env")):
         logger.setLevel(logging.NOTSET)
         logger.propagate = True
         logger.handlers.clear()
+
+
+@pytest.fixture(scope="session")
+def repo_path() -> Path:
+    this_dir = Path(__file__).resolve().parent
+    return this_dir.parent
+
+
+@pytest.fixture(scope="session")
+def fixture_dir(repo_path: Path) -> Path:
+    return repo_path / "tests" / "fixtures"
 
 
 @pytest.fixture()
@@ -31,8 +36,3 @@ def unittest_client() -> httpx.Client:
 def unittest_respx() -> Iterable[respx.MockRouter]:
     with respx.mock(base_url="https://example.com") as r:
         yield r
-
-
-@pytest.fixture()
-def fixture_dir(repo_path: Path) -> Path:
-    return repo_path / "tests" / "fixtures"
