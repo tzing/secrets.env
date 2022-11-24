8d546952402629af0c6664d09e7415001ee9c43f
tzing <tzingshih@gmail.com>
2022-11-24 23:18:44 +0800

    fix flake8

--- secrets_env/auth/__init__.py
+++ secrets_env/auth/__init__.py
@@ -20,7 +20,7 @@ def get_auth(name: str, data: dict) -> Optional[Auth]:
     elif name_ == "oidc":
         from secrets_env.auth.oidc import OpenIDConnectAuth
         return OpenIDConnectAuth.load(data)
-    elif name_ =='null':
+    elif name_ == "null":
         from secrets_env.auth.null import NoAuth
         return NoAuth.load(data)
     # fmt: on
