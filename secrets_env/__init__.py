2cb9c3fff1d752414465e237b87c2d94b3248cab
tzing <tzingshih@gmail.com>
2022-11-24 23:04:21 +0800

    add tests

--- secrets_env/__init__.py
+++ secrets_env/__init__.py
@@ -12,49 +12,44 @@ import secrets_env.exception
 logger = logging.getLogger(__name__)
 
 
-def load_secrets(config_file: Optional[pathlib.Path] = None) -> Dict[str, str]:
+def load_secrets(config_file: Optional[pathlib.Path]) -> Dict[str, Optional[str]]:
     """Load secrets from vault and put them to environment variable."""
+    # parse config
     config = secrets_env.config.load_config(config_file)
     if not config:
         # skip logging. already show error in `load_config`
         return {}
 
-    reader = secrets_env.core.KVReader(
-        url=config.url,
-        auth=config.auth,
-        ca_cert=config.tls.get("ca_cert"),
-        client_cert=config.tls.get("client_cert"),
-        client_key=config.tls.get("client_key"),
-    )
+    # read secrets
+    reader = secrets_env.core.KVReader(**config["client"])
 
     try:
-        secrets = reader.read_values(config.secret_specs.values())
+        secrets = reader.read_values(config["secrets"].values())
     except secrets_env.exception.AuthenticationError as e:
         logger.error(
             "<!important>\u26D4 Authentication error: %s. No secret loaded.", e.args[0]
         )
         return {}
 
+    # build env var to secret mapping
     output = {}
-    for name, spec in config.secret_specs.items():
-        value = secrets.get(spec)
-        if not value:
-            # skip logging. already show warning in `get_value`
-            continue
-
-        logger.debug("Loaded <data>$%s</data>", name)
+    num_loaded = 0
+    for name, spec in config["secrets"].items():
+        if value := secrets.get(spec):
+            num_loaded += 1
+            logger.debug("Loaded <data>$%s</data>", name)
         output[name] = value
 
-    if len(output) == len(config.secret_specs):
+    if len(config["secrets"]) == num_loaded:
         logger.info(
             "<!important>\U0001F511 <mark>%d</mark> secrets loaded", len(secrets)
         )
     else:
         logger.warning(
-            # NOTE need extra whitespace after the modifier
+            # NOTE need extra whitespace after the modifier (\uFE0F)
             "<!important>\u26A0\uFE0F  <error>%d</error> / %d secrets loaded",
-            len(output),
-            len(config.secret_specs),
+            num_loaded,
+            len(config["secrets"]),
         )
 
     return output
--- tests/test___init__.py
+++ tests/test___init__.py
@@ -1,69 +1,82 @@
 import logging
-from unittest.mock import Mock, patch
+from pathlib import Path
+from unittest.mock import Mock
 
 import pytest
 
 import secrets_env
-from secrets_env.auth.token import TokenAuth
+from secrets_env.auth.null import NoAuth
+from secrets_env.core import KVReader
 from secrets_env.exception import AuthenticationError
 
 
-class TestLoadSecrets:
-    @pytest.fixture()
-    def patch_load_config(self):
-        with patch(
-            "secrets_env.config.load_config",
-            return_value=Config(
-                url="https://example.com/",
-                auth=TokenAuth("ex@mp1e"),
-                tls={},
-                secret_specs={
-                    "VAR1": SecretPath("key1", "example"),
-                    "VAR2": SecretPath("key2", "example"),
-                },
-            ),
-        ) as mock:
-            yield mock
-
-    @pytest.mark.usefixtures("patch_load_config")
-    def test_success(self, caplog: pytest.LogCaptureFixture):
-        with patch(
-            "secrets_env.core.KVReader.read_values",
-            return_value={
-                SecretPath("key1", "example"): "foo",
-                SecretPath("key2", "example"): "bar",
+@pytest.fixture()
+def _patch_load_config(monkeypatch: pytest.MonkeyPatch):
+    def mock_load_config(path):
+        assert path is None or isinstance(path, Path)
+        return {
+            "client": {
+                "url": "https://example.com/",
+                "auth": NoAuth(),
+                "ca_cert": Path("/data/ca.cert"),
             },
-        ), caplog.at_level(logging.INFO):
-            assert secrets_env.load_secrets(None) == {
-                "VAR1": "foo",
-                "VAR2": "bar",
-            }
-
-        assert "<mark>2</mark> secrets loaded" in caplog.text
-
-    @pytest.mark.usefixtures("patch_load_config")
-    def test_partial_loaded(self, caplog: pytest.LogCaptureFixture):
-        with patch(
-            "secrets_env.core.KVReader.read_values",
-            return_value={
-                # no key2
-                SecretPath("key1", "example"): "foo",
+            "secrets": {
+                "VAR1": ("secrets/sample1", "foo"),
+                "VAR2": ("secrets/sample2", "bar"),
             },
-        ):
-            assert secrets_env.load_secrets(None) == {"VAR1": "foo"}
+        }
 
-        assert "<error>1</error> / 2 secrets loaded" in caplog.text
+    monkeypatch.setattr("secrets_env.config.load_config", mock_load_config)
 
-    def test_no_config(self, patch_load_config: Mock):
-        patch_load_config.return_value = None
-        assert secrets_env.load_secrets(None) == {}
 
-    @pytest.mark.usefixtures("patch_load_config")
-    def test_auth_error(self, caplog: pytest.LogCaptureFixture):
-        with patch(
-            "secrets_env.core.KVReader.read_values",
-            side_effect=AuthenticationError("test error"),
-        ):
-            assert secrets_env.load_secrets(None) == {}
+@pytest.mark.usefixtures("_patch_load_config")
+def test_success(monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
+    caplog.set_level(logging.INFO)
+    monkeypatch.setattr(
+        KVReader,
+        "read_values",
+        lambda _1, _2: {
+            ("secrets/sample1", "foo"): "secret-1",
+            ("secrets/sample2", "bar"): "secret-2",
+        },
+    )
 
-        assert "Authentication error: test error" in caplog.text
+    assert secrets_env.load_secrets(None) == {
+        "VAR1": "secret-1",
+        "VAR2": "secret-2",
+    }
+    assert "<mark>2</mark> secrets loaded" in caplog.text
+
+
+@pytest.mark.usefixtures("_patch_load_config")
+def test_partial_loaded(
+    monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture
+):
+    monkeypatch.setattr(
+        KVReader,
+        "read_values",
+        lambda _1, _2: {
+            ("secrets/sample1", "foo"): "secret-1",
+            ("secrets/sample2", "bar"): None,
+        },
+    )
+
+    assert secrets_env.load_secrets(None) == {
+        "VAR1": "secret-1",
+        "VAR2": None,
+    }
+    assert "<error>1</error> / 2 secrets loaded" in caplog.text
+
+
+def test_no_config(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr("secrets_env.config.load_config", lambda _: None)
+    assert secrets_env.load_secrets(None) == {}
+
+
+@pytest.mark.usefixtures("_patch_load_config")
+def test_auth_error(monkeypatch: pytest.MonkeyPatch, caplog: pytest.LogCaptureFixture):
+    monkeypatch.setattr(
+        KVReader, "read_values", Mock(side_effect=AuthenticationError("test error"))
+    )
+    assert secrets_env.load_secrets(None) == {}
+    assert "Authentication error: test error" in caplog.text
