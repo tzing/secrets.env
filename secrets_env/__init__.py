52e8e08fd9efccb93855b796f03638a6df0451fc
tzing <tzingshih@gmail.com>
2022-11-28 23:45:40 +0800

    add to init

--- secrets_env/__init__.py
+++ secrets_env/__init__.py
@@ -8,8 +8,10 @@ from typing import Dict, Optional
 import secrets_env.config
 import secrets_env.core
 import secrets_env.exception
+import secrets_env.plugins
 
 logger = logging.getLogger(__name__)
+hookimpl = secrets_env.plugins.hookimpl
 
 
 def load_secrets(
