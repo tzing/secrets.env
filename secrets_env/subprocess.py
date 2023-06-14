aa994d71f70e05e49692bb3642df9e7d160ec86c
tzing <tzingshih@gmail.com>
2023-06-14 20:35:12 +0800

    wip

---
+++ secrets_env/subprocess.py
@@ -0,0 +1,114 @@
+import enum
+import logging
+import queue
+import subprocess
+import threading
+import time
+from typing import IO, Iterator, List, Sequence, Tuple
+
+from secrets_env.utils import strip_ansi
+
+logger = logging.getLogger(__name__)
+
+
+class Channel(enum.IntEnum):
+    prefix: str
+
+    def __new__(cls, value: int, prefix: str) -> "Channel":
+        obj = int.__new__(cls, value)
+        obj._value_ = value
+        obj.prefix = prefix
+        return obj
+
+    Stdout = 1, "<"
+    Stderr = 2, "<[stderr]"
+
+
+class Run:
+    """Run subprocess and yields both stdout and stderr in real time."""
+
+    def __init__(self, cmd: Sequence[str]) -> None:
+        self._queue = queue.Queue()
+        self._stdouts: List[str] = []
+        self._stderrs: List[str] = []
+
+        # start process
+        logger.debug("$ %s", " ".join(cmd))
+
+        self._proc = subprocess.Popen(
+            args=cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            bufsize=1,
+            encoding="utf-8",
+        )
+
+        # fire threads to read output
+        threading.Thread(
+            target=polling_output,
+            args=(Channel.Stdout, self._proc.stdout, self._queue),
+            daemon=True,
+        ).start()
+
+        threading.Thread(
+            target=polling_output,
+            args=(Channel.Stderr, self._proc.stderr, self._queue),
+            daemon=True,
+        ).start()
+
+    def _iter_output(self) -> Iterator[Tuple[Channel, str]]:
+        POLL_INTERVAL = 0.1
+        while True:
+            try:
+                ch, line = self._queue.get_nowait()
+                if ch == Channel.Stdout:
+                    self._stdouts.append(line)
+                else:
+                    self._stderrs.append(line)
+                yield ch, line
+
+            except queue.Empty:
+                if self._proc.poll():
+                    break
+                else:
+                    time.sleep(POLL_INTERVAL)
+
+    def _flush(self):
+        for _ in self._iter_output():
+            ...
+
+    def iter_any_output(self) -> Iterator[str]:
+        for _, line in self._iter_output():
+            yield line
+
+    @property
+    def return_code(self):
+        return self._proc.wait()
+
+    @property
+    def stdout(self) -> str:
+        self._flush()
+        return "".join(self._stdouts)
+
+    @property
+    def stderr(self) -> str:
+        self._flush()
+        return "".join(self._stderrs)
+
+
+def polling_output(ch: Channel, source: IO[str], q: queue.Queue):
+    logger.debug(
+        "Subprocess polling worker created. thread id= %s; channel = %s",
+        threading.get_native_id(),
+        ch.name,
+    )
+
+    for line in source:
+        q.put((ch, line))
+        logger.debug("%s %s", ch.prefix, strip_ansi(line.rstrip()))
+
+    logger.debug(
+        "Subprocess polling worker shutdown. thread id= %s; channel = %s",
+        threading.get_native_id(),
+        ch.name,
+    )
