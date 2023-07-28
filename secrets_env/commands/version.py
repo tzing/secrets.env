1396eb0cd1643c4b28485e799fa0a69e0f7d494c
tzing <tzingshih@gmail.com>
2023-07-28 10:36:00 +0800

    remove shared __name__

--- secrets_env/commands/version.py
+++ secrets_env/commands/version.py
@@ -1,10 +1,10 @@
 import click
 
-from secrets_env._metadata import __name__, __version__
+from secrets_env._metadata import __version__
 from secrets_env.click import entrypoint
 
 
 @entrypoint.command()
 def version():
     """Show current version and status"""
-    click.echo(f"{__name__} {__version__}")
+    click.echo(__version__)
