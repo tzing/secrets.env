37b88717e6d6df86159eb61c2899ad23768dd2f0
tzing <tzingshih@gmail.com>
2023-08-07 22:48:47 +0800

    change: split config parser

--- tests/providers/teleport/test_teleport_config.py
+++ tests/providers/teleport/test_teleport_config.py
@@ -4,16 +4,36 @@ import secrets_env.providers.teleport.config as t
 from secrets_env.exceptions import ConfigError
 
 
+def test_parse_source_config():
+    assert t.parse_source_config({"app": "test"}) == {
+        "proxy": None,
+        "user": None,
+        "app": "test",
+    }
+
+
+def test_parse_adapter_config():
+    # success
+    assert t.parse_adapter_config({"teleport": "test"}) == {
+        "proxy": None,
+        "user": None,
+        "app": "test",
+    }
+
+    # failed - in adapter mode the configs must placed in 'teleport' section
+    with pytest.raises(ConfigError):
+        t.parse_adapter_config({"app": "test"})
+
+
 class TestParseConfig:
     def test_success(self):
         assert t.parse_config(
+            "test.teleport",
             {
-                "teleport": {
-                    "proxy": "example.com",
-                    "user": "user",
-                    "app": "test",
-                }
-            }
+                "proxy": "example.com",
+                "user": "user",
+                "app": "test",
+            },
         ) == {
             "proxy": "example.com",
             "user": "user",
@@ -21,7 +41,7 @@ class TestParseConfig:
         }
 
     def test_shortcut(self):
-        assert t.parse_config({"teleport": "test"}) == {
+        assert t.parse_config("test.teleport", "test") == {
             "proxy": None,
             "user": None,
             "app": "test",
@@ -29,8 +49,8 @@ class TestParseConfig:
 
     def test_type_error(self):
         with pytest.raises(ConfigError):
-            t.parse_config({"teleport": 1234})
+            t.parse_config("test.teleport", 1234)
 
     def test_no_config(self):
         with pytest.raises(ConfigError):
-            t.parse_config({})
+            t.parse_config("test.teleport", {})
