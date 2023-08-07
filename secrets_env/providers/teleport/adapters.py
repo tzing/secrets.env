5c301e69c24bb173f6f49c197623195fa9b04f9e
tzing <tzingshih@gmail.com>
2023-08-07 23:23:55 +0800

    add: handle adapter

--- secrets_env/providers/teleport/adapters.py
+++ secrets_env/providers/teleport/adapters.py
@@ -2,6 +2,8 @@ import logging
 import typing
 
 from secrets_env.exceptions import ConfigError
+from secrets_env.providers.teleport.config import parse_adapter_config
+from secrets_env.providers.teleport.helper import get_connection_info
 
 if typing.TYPE_CHECKING:
     from secrets_env.provider import ProviderBase
@@ -12,11 +14,24 @@ AdapterType = typing.Callable[[str, dict, "AppConnectionInfo"], "ProviderBase"]
 logger = logging.getLogger(__name__)
 
 
-def get_adapter(type_: str) -> AdapterType:
-    if type_ == "vault":
+def handle(subtype: str, data: dict) -> "ProviderBase":
+    # ensure the adopted provider type is supportted
+    adapter = get_adapter(subtype)
+
+    # get connection parameter
+    app_param = parse_adapter_config(data)
+    conn_info = get_connection_info(app_param)
+
+    # forward parameters to corresponding provider
+    return adapter(subtype, data, conn_info)
+
+
+def get_adapter(name: str) -> AdapterType:
+    iname = name.lower()
+    if iname == "vault":
         return adapt_vault_provider
 
-    raise ConfigError("Unknown provider type {}", type_)
+    raise ConfigError("Unknown provider type {}", name)
 
 
 def adapt_vault_provider(type_: str, data: dict, conn_info: "AppConnectionInfo"):
--- tests/providers/teleport/test_teleport_adapters.py
+++ tests/providers/teleport/test_teleport_adapters.py
@@ -6,11 +6,32 @@ import pytest
 import secrets_env.providers.teleport.adapters as t
 from secrets_env.exceptions import ConfigError
 from secrets_env.provider import ProviderBase
+from secrets_env.providers.teleport.config import AppParameter
 from secrets_env.providers.teleport.helper import AppConnectionInfo
 
 
+def test_handle(monkeypatch: pytest.MonkeyPatch):
+    def mock_get_adapter(name):
+        assert name == "Test"
+        return Mock(return_value={"test": "result"})
+
+    def mock_parse_adapter_config(data):
+        assert isinstance(data, dict)
+        return Mock(spec=AppParameter)
+
+    def mock_get_connection_info(param):
+        assert isinstance(param, dict)
+        return Mock(spec=AppConnectionInfo)
+
+    monkeypatch.setattr(t, "get_adapter", mock_get_adapter)
+    monkeypatch.setattr(t, "parse_adapter_config", mock_parse_adapter_config)
+    monkeypatch.setattr(t, "get_connection_info", mock_get_connection_info)
+
+    assert t.handle("Test", {"test": "input"}) == {"test": "result"}
+
+
 def test_get_adapter():
-    assert callable(t.get_adapter("vault"))
+    assert callable(t.get_adapter("Vault"))
 
     with pytest.raises(ConfigError):
         t.get_adapter("no-this-type")
