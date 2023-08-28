f0034a90b3fc4846f931a8090d7bf5d31ae5a111
tzing <tzingshih@gmail.com>
2023-08-29 00:14:50 +0800

    register provider

--- secrets_env/providers/__init__.py
+++ secrets_env/providers/__init__.py
@@ -13,18 +13,21 @@ def get_provider(data: dict) -> "ProviderBase":
     type_lower = type_.lower()
 
     # fmt: off
-    if type_lower.startswith("teleport+"):
-        from . import teleport
-        return teleport.get_provider(type_, data)
     if type_lower == "null":
         from . import null
         return null.get_provider(type_, data)
     if type_lower == "plain":
         from . import plain
         return plain.get_provider(type_, data)
+    if type_lower == "teleport":
+        from . import teleport
+        return teleport.get_provider(type_, data)
     if type_lower == "vault":
         from . import vault
         return vault.get_provider(type_, data)
+    if type_lower.startswith("teleport+"):
+        from . import teleport
+        return teleport.get_adapted_provider(type_, data)
     # fmt: on
 
     raise secrets_env.exceptions.ConfigError("Unknown provider type {}", type_)
