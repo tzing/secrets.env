663dc09f4bfb09f1c4ea3313e65e2fd3c9ed1288
tzing <tzingshih@gmail.com>
2023-08-28 23:57:57 +0800

    add: get_provider

--- secrets_env/providers/teleport/__init__.py
+++ secrets_env/providers/teleport/__init__.py
@@ -4,20 +4,34 @@ from secrets_env.exceptions import ConfigError
 
 if typing.TYPE_CHECKING:
     from secrets_env.provider import ProviderBase
+    from secrets_env.providers.teleport.provider import TeleportProvider
 
-PROVIDER_NAME = "teleport"
 ADAPTER_PREFIX = "teleport+"
 
 
-def get_provider(type_: str, data: dict) -> "ProviderBase":
+def get_provider(type_: str, data: dict) -> "TeleportProvider":
+    from .config import parse_source_config
+    from .provider import TeleportProvider
+
+    cfg = parse_source_config(data)
+    return TeleportProvider(**cfg)
+
+
+def get_adapted_provider(type_: str, data: dict) -> "ProviderBase":
+    from .adapters import get_adapter
+    from .config import parse_adapter_config
+    from .helper import get_connection_info
+
     iname = type_.lower()
-    if iname == PROVIDER_NAME:
-        raise NotImplementedError
+    if not iname.startswith(ADAPTER_PREFIX):
+        raise ConfigError("Not a Teleport compatible provider: {}", type_)
 
-    if iname.startswith(ADAPTER_PREFIX):
-        from .adapters import handle
+    subtype = type_[len(ADAPTER_PREFIX) :]
+    factory = get_adapter(subtype)
 
-        subtype = type_[len(ADAPTER_PREFIX) :]
-        return handle(subtype, data)
+    # get connection parameter
+    app_param = parse_adapter_config(data)
+    conn_info = get_connection_info(app_param)
 
-    raise ConfigError("Not a Teleport compatible provider: {}", type_)
+    # forward parameters to corresponding provider
+    return factory(subtype, data, conn_info)
