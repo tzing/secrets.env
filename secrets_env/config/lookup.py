30cc19f3e63ff574d5672997ffeb6363733cab51
tzing <tzingshih@gmail.com>
2023-11-08 21:50:23 +0800

    add: find_local_config_file

---
+++ secrets_env/config/lookup.py
@@ -0,0 +1,83 @@
+from __future__ import annotations
+
+import importlib
+import importlib.util
+import itertools
+import logging
+import typing
+from dataclasses import dataclass
+from pathlib import Path
+
+import platformdirs
+
+if typing.TYPE_CHECKING:
+    from typing import Literal
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True)
+class ConfigFileSpec:
+    """Avaliable config file formats."""
+
+    filename: str
+    format: Literal["toml", "yaml", "json", "pyproject.toml"]
+
+
+@dataclass(frozen=True)
+class ConfigFile(ConfigFileSpec):
+    path: Path
+
+
+def find_local_config_file(cwd: Path | None = None) -> ConfigFile | None:
+    """Find config file in current directory."""
+    if cwd is None:
+        cwd = Path.cwd()
+
+    CONFIG_FILE_FORMATS = (
+        ConfigFileSpec(".secrets-env.toml", "toml"),
+        ConfigFileSpec(".secrets-env.yaml", "yaml"),
+        ConfigFileSpec(".secrets-env.yml", "yaml"),
+        ConfigFileSpec(".secrets-env.json", "json"),
+        ConfigFileSpec("pyproject.toml", "pyproject.toml"),
+    )
+
+    for dir_ in itertools.chain([cwd], cwd.parents):
+        for spec in CONFIG_FILE_FORMATS:
+            path = dir_ / spec.filename
+            if not path.is_file():
+                continue
+
+            logger.debug("Find config file %s", path)
+
+            if not is_readable_format(spec.format):
+                logger.warning(
+                    "The config file %s was found, but the required dependency "
+                    "for %s format is not installed.",
+                    path,
+                    spec.format,
+                )
+                continue
+
+            return ConfigFile(spec.filename, spec.format, path)
+
+    return None
+
+
+def is_readable_format(fmt: str) -> bool:
+    if fmt == "json":
+        return True
+    elif fmt == "yaml":
+        return is_installed("yaml")
+    elif fmt in ("toml", "pyproject.toml"):
+        return is_installed("tomllib") or is_installed("tomli")
+    return False
+
+
+def is_installed(module: str) -> bool:
+    try:
+        if importlib.util.find_spec(module):
+            return True
+    except ImportError:
+        ...
+    return False
---
+++ tests/config/test_lookup.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+
+import pytest
+
+import secrets_env.config.lookup as t
+
+
+class TestFindLocalConfigFile:
+    @pytest.mark.parametrize(
+        ("filename", "format_"),
+        [
+            (".secrets-env.toml", "toml"),
+            (".secrets-env.yaml", "yaml"),
+            (".secrets-env.yml", "yaml"),
+            (".secrets-env.json", "json"),
+            ("pyproject.toml", "pyproject.toml"),
+        ],
+    )
+    def test_success(
+        self,
+        monkeypatch: pytest.MonkeyPatch,
+        tmp_path: Path,
+        filename: str,
+        format_: str,
+    ):
+        monkeypatch.chdir(tmp_path)
+        (tmp_path / filename).touch()
+
+        file = t.find_local_config_file()
+        assert isinstance(file, t.ConfigFile)
+        assert isinstance(file.path, Path)
+        assert file.filename == filename
+        assert file.format == format_
+
+    def test_multiple(self, tmp_path: Path):
+        (tmp_path / ".secrets-env.toml").touch()
+        (tmp_path / ".secrets-env.json").touch()
+
+        file = t.find_local_config_file(tmp_path)
+        assert isinstance(file, t.ConfigFile)
+        assert file.filename == ".secrets-env.toml"
+
+    def test_not_readable(
+        self,
+        monkeypatch: pytest.MonkeyPatch,
+        tmp_path: Path,
+        caplog: pytest.LogCaptureFixture,
+    ):
+        monkeypatch.setattr(t, "is_readable_format", lambda _: False)
+        (tmp_path / ".secrets-env.toml").touch()
+
+        file = t.find_local_config_file(tmp_path)
+        assert file is None
+        assert "the required dependency for toml format is not installed" in caplog.text
+
+
+def test_is_readable_format():
+    assert t.is_readable_format("json") is True
+    assert t.is_readable_format("yaml") is True
+    assert t.is_readable_format("toml") is True
+    assert t.is_readable_format("pyproject.toml") is True
+    assert t.is_readable_format("html") is False
+
+
+def test_is_installed():
+    assert t.is_installed("json") is True
+    assert t.is_installed(".error") is False
