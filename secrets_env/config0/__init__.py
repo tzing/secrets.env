4155e7a79ed2208142923e140b555609e15ad3cd
tzing <tzingshih@gmail.com>
2023-12-17 18:25:27 +0800

    change: use new reader

--- secrets_env/config0/__init__.py
+++ secrets_env/config0/__init__.py
@@ -2,9 +2,9 @@ import logging
 import typing
 from typing import Optional
 
-from secrets_env.config0.finder import find_config_file, get_config_file_metadata
 from secrets_env.config0.parser import parse_config
-from secrets_env.config0.reader import read_config_file
+from secrets_env.config.lookup import find_local_config_file
+from secrets_env.config.reader import read
 
 if typing.TYPE_CHECKING:
     from pathlib import Path
@@ -16,16 +16,14 @@ logger = logging.getLogger(__name__)
 
 def load_config(path: Optional["Path"] = None) -> Optional["Config"]:
     """Load the configurations and formated in to the typed structure."""
-    if path:
-        file_metadata = get_config_file_metadata(path)
-    else:
-        file_metadata = find_config_file()
+    if not path:
+        path = find_local_config_file()
 
-    if not file_metadata:
+    if not path:
         logger.info("Config file not found.")
         return None
 
-    logger.info("Read secrets.env config from <data>%s</data>", file_metadata.path)
+    logger.info("Read secrets.env config from <data>%s</data>", path)
 
-    data = read_config_file(file_metadata)
+    data = read(path)
     return parse_config(data)
