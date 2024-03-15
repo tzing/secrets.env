0a05bb9f481acb781a380034560f29f7decce827
tzing <tzingshih@gmail.com>
2024-03-16 00:51:36 +0800

    wip

--- secrets_env/providers/vault/provider.py
+++ secrets_env/providers/vault/provider.py
@@ -8,11 +8,14 @@ import httpx
 from pydantic import BaseModel, model_validator
 
 import secrets_env.version
+from secrets_env.exceptions import AuthenticationError
 from secrets_env.provider import Provider
 from secrets_env.providers.vault.config import VaultUserConfig
+from secrets_env.utils import get_httpx_error_reason, log_httpx_response
 
 if typing.TYPE_CHECKING:
     from secrets_env.provider import RequestSpec
+    from secrets_env.providers.vault.auth import Auth
 
 logger = logging.getLogger(__name__)
 
@@ -66,3 +69,52 @@ def create_http_client(config: VaultUserConfig) -> httpx.Client:
         client_params["cert"] = config.tls.client_cert
 
     return httpx.Client(**client_params)
+
+
+def get_token(client: httpx.Client, auth: Auth) -> str:
+    """
+    Request a token from the Vault server and verify it.
+
+    Raises
+    ------
+    AuthenticationError
+        If the token cannot be retrieved or is invalid.
+    """
+    # login
+    try:
+        token = auth.login(client)
+    except httpx.HTTPError as e:
+        if not (reason := get_httpx_error_reason(e)):
+            raise
+        raise AuthenticationError("Encounter {} while retrieving token", reason) from e
+
+    if not token:
+        raise AuthenticationError("Absence of token information")
+
+    # verify
+    if not is_authenticated(client, token):
+        raise AuthenticationError("Invalid token")
+
+    return token
+
+
+def is_authenticated(client: httpx.Client, token: str) -> bool:
+    """Check is a token is authenticated.
+
+    See also
+    --------
+    https://developer.hashicorp.com/vault/api-docs/auth/token
+    """
+    logger.debug("Validate token for %s", client.base_url)
+
+    resp = client.get("/v1/auth/token/lookup-self", headers={"X-Vault-Token": token})
+    if resp.is_success:
+        return True
+
+    logger.debug(
+        "Token verification failed. Code= %d (%s). Msg= %s",
+        resp.status_code,
+        resp.reason_phrase,
+        resp.json(),
+    )
+    return False
--- tests/providers/vault/test_vault_provider.py
+++ tests/providers/vault/test_vault_provider.py
@@ -1,11 +1,13 @@
+import os
 from pathlib import Path
 from unittest.mock import Mock
 
 import httpx
 import pytest
+import respx
 
 from secrets_env.providers.vault.config import TlsConfig, VaultUserConfig
-from secrets_env.providers.vault.provider import create_http_client
+from secrets_env.providers.vault.provider import create_http_client, is_authenticated
 
 
 class TestCreateHttpClient:
@@ -86,3 +88,35 @@ class TestCreateHttpClient:
 
         _, kwargs = mock_httpx_client.call_args
         assert kwargs["cert"] == (Path("/mock/client.pem"), Path("/mock/client.key"))
+
+
+class TestGetToken: ...  # FIXME
+
+
+class TestIsAuthenticated:
+
+    def test_success(self, respx_mock: respx.MockRouter):
+        respx_mock.get("https://vault.example.com/v1/auth/token/lookup-self")
+
+        client = httpx.Client(base_url="https://vault.example.com")
+        assert is_authenticated(client, "test-token") is True
+
+    def test_fail(self, respx_mock: respx.MockRouter):
+        respx_mock.get("https://vault.example.com/v1/auth/token/lookup-self").respond(
+            status_code=403,
+            json={"errors": ["mock permission denied"]},
+        )
+
+        client = httpx.Client(base_url="https://vault.example.com")
+        assert is_authenticated(client, "test-token") is False
+
+    def test_integration(self):
+        url = os.getenv("VAULT_ADDR")
+        token = os.getenv("VAULT_TOKEN")
+        if not url or not token:
+            pytest.skip("VAULT_ADDR or VAULT_TOKEN are not set")
+
+        client = httpx.Client(base_url=url)
+
+        assert is_authenticated(client, token) is True
+        assert is_authenticated(client, "invalid-token") is False
