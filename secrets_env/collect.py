ea2383867d200c87e85dbbc8581dc0d7bf28eb3b
Tzu-Ting <tzingshih@gmail.com>
2024-04-08 20:36:12 +0800

    chore: errors (#157)

--- secrets_env/collect.py
+++ secrets_env/collect.py
@@ -64,7 +64,7 @@ def read1(provider: Provider, name: str, spec: Request) -> str | None:
         )
     except secrets_env.exceptions.ConfigError as e:
         logger.warning("Config for %s is malformed: %s. Skip this variable.", name, e)
-    except secrets_env.exceptions.ValueNotFound:
+    except LookupError:
         logger.warning("Secret for %s not found. Skip this variable.", name)
     except Exception as e:
         logger.error("Error requesting secret for %s. Skip this variable.", name)
