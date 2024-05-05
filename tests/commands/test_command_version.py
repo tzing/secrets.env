83b9d4ba19dd18726e4a2a613fbd2f3d0adabf64
tzing <tzingshih@gmail.com>
2024-05-05 22:49:20 +0800

    drop version

--- tests/commands/test_command_version.py
+++
@@ -1,9 +0,0 @@
-import click.testing
-
-import secrets_env.commands.version as t
-
-
-def test():
-    runner = click.testing.CliRunner()
-    result = runner.invoke(t.version)
-    assert result.exit_code == 0
