7a3bbe2fc1658f7a46fea63831d8944f2ab66b2c
tzing <tzingshih@gmail.com>
2024-05-09 23:31:28 +0800

    fix test

--- tests/providers/vault/auth/test_token.py
+++ tests/providers/vault/auth/test_token.py
@@ -1,45 +1,31 @@
-from pathlib import Path
 from unittest.mock import Mock
 
 import httpx
 import pytest
-from pydantic_core import Url
+from pydantic_core import Url, ValidationError
 
 import secrets_env.providers.vault.auth.token as t
 from secrets_env.providers.vault.auth.token import TokenAuth
 
 
 class TestTokenAuth:
-    @pytest.fixture(autouse=True)
-    def _disable_env(self, monkeypatch: pytest.MonkeyPatch):
-        monkeypatch.delenv("VAULT_TOKEN", False)
-
-    @pytest.fixture()
-    def _disable_token_helper(self, monkeypatch: pytest.MonkeyPatch, tmp_path: Path):
-        monkeypatch.setattr(Path, "home", lambda: tmp_path)
-
-    def setup_method(self):
-        self.sample = t.TokenAuth(token="T0ken")
-
     def test_create_from_envvar(self, monkeypatch: pytest.MonkeyPatch):
+        sample = t.TokenAuth(token="T0ken")
+
         with monkeypatch.context() as m:
             m.setenv("SECRETS_ENV_TOKEN", "T0ken")
-            assert t.TokenAuth.create(Url("https://example.com/"), {}) == self.sample
+            assert t.TokenAuth.create(Url("https://example.com/"), {}) == sample
 
         with monkeypatch.context() as m:
             m.setenv("VAULT_TOKEN", "T0ken")
-            assert t.TokenAuth.create(Url("https://example.com/"), {}) == self.sample
+            assert t.TokenAuth.create(Url("https://example.com/"), {}) == sample
 
-    def test_create_from_helper(self, monkeypatch: pytest.MonkeyPatch, tmp_path: Path):
-        (tmp_path / ".vault-token").write_text("T0ken")
-        monkeypatch.setattr(Path, "home", lambda: tmp_path)
-        assert TokenAuth.create(Url("https://example.com/"), {}) == self.sample
-
-    @pytest.mark.usefixtures("_disable_token_helper")
-    def test_create_failed(self):
-        with pytest.raises(ValueError, match="Missing token for Vault authentication."):
+    def test_create_failed(self, monkeypatch: pytest.MonkeyPatch):
+        monkeypatch.delenv("VAULT_TOKEN", False)
+        with pytest.raises(ValidationError, match="Input should be a valid string."):
             assert TokenAuth.create(Url("https://example.com/"), {}) is None
 
     def test_login(self):
         client = Mock(spec=httpx.Client)
-        assert self.sample.login(client) == "T0ken"
+        sample = t.TokenAuth(token="T0ken")
+        assert sample.login(client) == "T0ken"
