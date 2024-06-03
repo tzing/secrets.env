e99f94f7903fa0a2a37859c4d9188f31ce124432
tzing <tzingshih@gmail.com>
2024-06-04 00:17:06 +0800

    move code

--- secrets_env/utils.py
+++ secrets_env/utils.py
@@ -14,8 +14,6 @@ import warnings
 from pathlib import Path
 from typing import TypeVar, overload
 
-from secrets_env.exceptions import OperationError
-
 if typing.TYPE_CHECKING:
     import click
     import httpx
@@ -227,52 +225,3 @@ def _show_warning(
         s = warnings.formatwarning(message, category, filename, lineno, line)
         logger = logging.getLogger("py.warnings")
         logger.warning(str(s))
-
-
-def detect_shell() -> tuple[str, Path]:
-    """
-    Detect the current shell.
-
-    Returns
-    -------
-    shell : str
-        Shell name.
-    path : Path
-        Path to the shell executable.
-
-    Raises
-    ------
-    OperationError
-        If the shell cannot be detected.
-    """
-    # try to detect shell via shellingham
-    if pair := _detect_shell_via_shellingham():
-        return pair
-
-    # fallback to default
-    raw_shell_path = None
-    if os.name == "posix":
-        raw_shell_path = os.getenv("SHELL")
-    elif os.name == "nt":
-        raw_shell_path = os.getenv("COMSPEC")
-
-    if not raw_shell_path:
-        raise OperationError("Cannot detect shell")
-
-    shell_path = Path(raw_shell_path)
-    logger.debug("Use default shell: %s", shell_path)
-    return shell_path.stem.lower(), shell_path
-
-
-def _detect_shell_via_shellingham() -> tuple[str, Path] | None:
-    try:
-        import shellingham
-    except ImportError:
-        return None
-
-    try:
-        shell, path = shellingham.detect_shell()
-        logger.debug("Detect shell: %s", path)
-        return shell, Path(path)
-    except shellingham.ShellDetectionFailure:
-        return None
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,4 @@
 import logging
-import os
 import sys
 import warnings
 from pathlib import Path
@@ -9,7 +8,6 @@ import click
 import httpx
 import keyring.errors
 import pytest
-import shellingham
 from pydantic_core import Url
 
 import secrets_env.utils as t
@@ -164,45 +162,3 @@ class TestSetupCaptureWarnings:
             warnings.warn("test warning", UserWarning, stacklevel=1)
 
         assert "UserWarning: test warning" in caplog.text
-
-
-class TestDetectShell:
-    def test_success(self):
-        shell, path = t.detect_shell()
-        assert isinstance(shell, str)
-        assert isinstance(path, Path)
-
-    @pytest.mark.skipif(
-        os.getenv("CI") is None, reason="Expect GitHub Action environment"
-    )
-    def test_success__ci(self):
-        shell, path = t.detect_shell()
-        assert shell == "bash"
-        assert path == Path("/usr/bin/bash")
-
-    @pytest.mark.skipif(
-        os.getenv("SHELL") != "/bin/zsh", reason="This is a hard-coded test for zsh"
-    )
-    def test_success__local(self):
-        """Expect local shell is zsh"""
-        shell, path = t.detect_shell()
-        assert shell == "zsh"
-        assert path == Path("/bin/zsh")
-
-    def test_fallback(self, monkeypatch: pytest.MonkeyPatch):
-        monkeypatch.setattr(t, "_detect_shell_via_shellingham", lambda: None)
-        monkeypatch.setenv("SHELL", "/bin/sh")
-        shell, path = t.detect_shell()
-        assert shell == "sh"
-        assert path == Path("/bin/sh")
-
-    def test_shellingham_not_installed(self, monkeypatch: pytest.MonkeyPatch):
-        monkeypatch.setattr("builtins.__import__", Mock(side_effect=ImportError))
-        assert t._detect_shell_via_shellingham() is None
-
-    def test_shellingham_error(self, monkeypatch: pytest.MonkeyPatch):
-        monkeypatch.setattr(
-            "shellingham.detect_shell",
-            Mock(side_effect=shellingham.ShellDetectionFailure),
-        )
-        assert t._detect_shell_via_shellingham() is None
