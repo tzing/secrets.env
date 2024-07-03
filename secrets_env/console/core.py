5325184f529b257aa191c9167199af29c931d9e7
tzing <tzingshih@gmail.com>
2024-07-03 22:55:58 +0800

    add: ExitCode

--- secrets_env/console/core.py
+++ secrets_env/console/core.py
@@ -1,11 +1,37 @@
 from __future__ import annotations
 
-__all__ = ["entrypoint", "with_output_options"]
+__all__ = ["ExitCode", "entrypoint", "exit", "with_output_options"]
+
+import enum
+import sys
+import typing
 
 import click
 
 import secrets_env.version
 from secrets_env.console.decorators import with_output_options
+
+if typing.TYPE_CHECKING:
+    from typing import NoReturn
+
+
+class ExitCode(enum.IntEnum):
+    Success = 0
+    """The command was successful."""
+
+    Error = 1
+    """An error occurred."""
+
+    ValueNotFound = 2
+    """Failed to load value."""
+
+    NestedEnvironment = 3
+    """Nested environment detected."""
+
+
+def exit(code: int) -> NoReturn:
+    # just in case `site.exit()` is not available
+    sys.exit(code)
 
 
 @click.group(
