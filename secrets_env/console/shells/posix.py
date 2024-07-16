166039e2076f9092e8c937273e3414fbc2604d50
tzing <tzingshih@gmail.com>
2024-07-16 20:57:51 +0800

    wip: activate script

--- secrets_env/console/shells/posix.py
+++ secrets_env/console/shells/posix.py
@@ -2,13 +2,16 @@ from __future__ import annotations
 
 import logging
 import os
+import shlex
 import shutil
 import signal
+import tempfile
 import typing
 
 from secrets_env.console.shells.base import Shell
 
 if typing.TYPE_CHECKING:
+    from pathlib import Path
     from types import FrameType
     from typing import NoReturn
 
@@ -18,6 +21,9 @@ logger = logging.getLogger(__name__)
 
 
 class PosixShell(Shell):
+    def __init__(self, shell_path: Path) -> None:
+        super().__init__(shell_path)
+        self.script_suffix = ".sh"
 
     def handover(self) -> int | None:
         try:
@@ -54,7 +60,26 @@ class PosixShell(Shell):
         return proc.signalstatus
 
     def do_post_spawn(self, proc: spawn) -> None:
+        # register signal handler for window resize
         register_window_resize(proc)
+
+        # prepare activation script
+        script_path = create_temporary_script(self.script_suffix)
+
+        with open(script_path, "w") as fd:
+            # transfer current environment
+            for key, value in os.environ.items():
+                print(f"{key}='{value}'", file=fd)
+                print(f"export {key}", file=fd)
+
+            # ask the shell to send us a signal when it finishes
+            print(f"kill -USR1 {os.getpid()}", file=fd)
+
+        # source the script
+        self._source_script(proc, script_path)
+
+    def _source_script(self, proc: spawn, script_path: str) -> None:
+        proc.sendline(f". {shlex.quote(script_path)}")
 
 
 def register_window_resize(proc: spawn) -> None:
@@ -68,3 +93,25 @@ def register_window_resize(proc: spawn) -> None:
         proc.setwinsize(dims.lines, dims.columns)
 
     signal.signal(signal.SIGWINCH, sigwinch_handler)
+
+
+def create_temporary_script(suffix: str) -> str:
+    """
+    Create a temporary file and delete it when receiving SIGUSR1.
+    """
+    fd, filename = tempfile.mkstemp(prefix="secrets-env-", suffix=suffix)
+    os.close(fd)
+
+    logger.debug("Created temporary script %s", filename)
+
+    def sigusr1_handler(sig: int, data: FrameType | None):
+        nonlocal filename
+        logger.debug("Received SIGUSR1, removing %s", filename)
+        try:
+            os.remove(filename)
+        except FileNotFoundError:
+            ...
+
+    signal.signal(signal.SIGUSR1, sigusr1_handler)
+
+    return filename
