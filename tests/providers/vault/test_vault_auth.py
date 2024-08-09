2d9eed3e38a4934c395d7a2a4e6c57300f408707
tzing <tzingshih@gmail.com>
2024-08-09 12:56:53 +0800

    mv: token

--- tests/providers/vault/test_vault_auth.py
+++ tests/providers/vault/test_vault_auth.py
@@ -1,10 +1,12 @@
 from unittest.mock import Mock
 
+import httpx
 import pytest
-from pydantic_core import Url
+from pydantic_core import Url, ValidationError
 
 from secrets_env.providers.vault.auth import create_auth_by_name
 from secrets_env.providers.vault.auth.base import Auth, NoAuth
+from secrets_env.providers.vault.auth.token import TokenAuth
 
 
 @pytest.mark.parametrize(
@@ -40,3 +42,26 @@ class TestNoAuth:
 
     def test_create(self):
         assert isinstance(NoAuth.create(Url("https://example.com/"), {}), NoAuth)
+
+
+class TestTokenAuth:
+    def test_create_from_envvar(self, monkeypatch: pytest.MonkeyPatch):
+        sample = TokenAuth(token="T0ken")
+
+        with monkeypatch.context() as m:
+            m.setenv("SECRETS_ENV_TOKEN", "T0ken")
+            assert TokenAuth.create(Url("https://example.com/"), {}) == sample
+
+        with monkeypatch.context() as m:
+            m.setenv("VAULT_TOKEN", "T0ken")
+            assert TokenAuth.create(Url("https://example.com/"), {}) == sample
+
+    def test_create_failed(self, monkeypatch: pytest.MonkeyPatch):
+        monkeypatch.delenv("VAULT_TOKEN", False)
+        with pytest.raises(ValidationError, match="Input should be a valid string."):
+            assert TokenAuth.create(Url("https://example.com/"), {}) is None
+
+    def test_login(self):
+        client = Mock(spec=httpx.Client)
+        sample = TokenAuth(token="T0ken")
+        assert sample.login(client) == "T0ken"
