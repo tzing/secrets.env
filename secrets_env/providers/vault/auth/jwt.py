78c854b0a5d60a9d06ae04e9e9b37ef7081db825
tzing <tzingshih@gmail.com>
2024-08-12 21:18:40 +0800

    fix lint

--- secrets_env/providers/vault/auth/jwt.py
+++ secrets_env/providers/vault/auth/jwt.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import logging
 from typing import TYPE_CHECKING, ClassVar
 
-from pydantic import SecretStr
+from pydantic import SecretStr  # noqa: TCH002
 
 from secrets_env.exceptions import AuthenticationError
 from secrets_env.providers.vault.auth.base import Auth
