7414d5dac1f03d4fb3f958355bb6850e3d64c1bc
tzing <tzingshih@gmail.com>
2024-09-02 23:31:45 +0800

    add: check_output

--- secrets_env/realms/subprocess.py
+++ secrets_env/realms/subprocess.py
@@ -7,13 +7,53 @@ import subprocess
 import typing
 
 if typing.TYPE_CHECKING:
-    from typing import Sequence
+    from typing import Literal, Sequence
 
 logger = logging.getLogger(__name__)
 regex_ansi = re.compile(r"\033\[[;?0-9]*[a-zA-Z]")
 
 
-def log_output(channel: str, message: str, level: int = logging.DEBUG):
+def check_output(commands: Sequence[str], *, write_output: bool = True) -> str:
+    """
+    A wrapper around :py:func:`subprocess.check_output` that logs the command
+    and output, and returns the output as a string.
+
+    Parameters
+    ----------
+    commands : list[str]
+        A list of commands to run.
+    write_output : bool, optional
+        Whether to write the output to the log, by default True.
+
+    Returns
+    -------
+    str
+        The output of the command.
+    """
+
+    def _quote(s) -> str:
+        return shlex.quote(strip_ansi(str(s)))
+
+    logger.debug("$ %s", " ".join(map(_quote, commands)))
+
+    try:
+        stdout = subprocess.check_output(
+            commands, stderr=subprocess.PIPE, encoding="utf-8"
+        )
+    except subprocess.CalledProcessError as e:
+        logging.debug("< return code: %d", e.returncode)
+        log_output("stderr", e.stderr)
+        raise
+
+    if write_output:
+        log_output("stdout", stdout)
+
+    return stdout
+
+
+def log_output(
+    channel: Literal["stdout", "stderr"], message: str, level: int = logging.DEBUG
+):
     """Write the output to the log."""
     message = strip_ansi(message.rstrip())
     for line in message.splitlines():
--- tests/realms/test_subprocess.py
+++ tests/realms/test_subprocess.py
@@ -1,8 +1,29 @@
 import logging
+import subprocess
 
 import pytest
 
 from secrets_env.realms.subprocess import check_output, log_output
+
+
+class TestCheckOutput:
+    def test_success(self, caplog: pytest.LogCaptureFixture):
+        with caplog.at_level(logging.DEBUG):
+            output = check_output(["printf", "\x1b[32mHello Secrets.env!\x1b[39m"])
+
+        assert output == "\x1b[32mHello Secrets.env!\x1b[39m"
+        assert "$ printf 'Hello Secrets.env!'" in caplog.text
+        assert "<[stdout] Hello Secrets.env!" in caplog.text
+
+    def test_failed(self, caplog: pytest.LogCaptureFixture):
+        with (
+            caplog.at_level(logging.DEBUG),
+            pytest.raises(subprocess.CalledProcessError),
+        ):
+            check_output(["sh", "-c", "echo Ah! > /dev/stderr && false"])
+
+        assert "< return code: 1" in caplog.text
+        assert "<[stderr] Ah!" in caplog.text
 
 
 class TestLogOutput:
