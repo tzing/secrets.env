d76d9cfb058afbec1639ecb30b0dc244acb5b259
tzing <tzingshih@gmail.com>
2024-09-02 23:50:40 +0800

    enh: use run

--- secrets_env/realms/subprocess.py
+++ secrets_env/realms/subprocess.py
@@ -13,7 +13,12 @@ logger = logging.getLogger(__name__)
 regex_ansi = re.compile(r"\033\[[;?0-9]*[a-zA-Z]")
 
 
-def check_output(commands: Sequence[str], *, write_output: bool = True) -> str:
+def check_output(
+    commands: Sequence[str],
+    *,
+    level_output: int | None = logging.DEBUG,
+    level_error: int | None = logging.ERROR,
+) -> str:
     """
     A wrapper around :py:func:`subprocess.check_output` that logs the command
     and output, and returns the output as a string.
@@ -22,13 +27,22 @@ def check_output(commands: Sequence[str], *, write_output: bool = True) -> str:
     ----------
     commands : list[str]
         A list of commands to run.
-    write_output : bool, optional
-        Whether to write the output to the log, by default True.
+    level_output : int, optional
+        The logging level to use for the output, by default :py:data:`logging.DEBUG`.
+        Set to :py:obj:`None` to disable logging the output.
+    level_error : int, optional
+        The logging level to use for the error, by default :py:data:`logging.ERROR`.
+        Set to :py:obj:`None` to disable logging the error.
 
     Returns
     -------
     str
         The output of the command.
+
+    Raises
+    ------
+    subprocess.CalledProcessError
+        If the command exits with a non-zero status.
     """
 
     def _quote(s) -> str:
@@ -37,24 +51,32 @@ def check_output(commands: Sequence[str], *, write_output: bool = True) -> str:
     logger.debug("$ %s", " ".join(map(_quote, commands)))
 
     try:
-        stdout = subprocess.check_output(
-            commands, stderr=subprocess.PIPE, encoding="utf-8"
+        proc = subprocess.run(
+            args=commands,
+            capture_output=True,
+            check=True,
+            encoding="utf-8",
         )
     except subprocess.CalledProcessError as e:
         logging.debug("< return code: %d", e.returncode)
-        log_output("stderr", e.stderr)
+        write_output("stdout", e.stdout, level_error)
+        write_output("stderr", e.stderr, level_error)
         raise
 
-    if write_output:
-        log_output("stdout", stdout)
+    logging.debug("< return code: %d", proc.returncode)
+    write_output("stdout", proc.stdout, level_output)
+    write_output("stderr", proc.stderr, level_output)
+    return proc.stdout
 
-    return stdout
 
-
-def log_output(
-    channel: Literal["stdout", "stderr"], message: str, level: int = logging.DEBUG
+def write_output(
+    channel: Literal["stdout", "stderr"],
+    message: str,
+    level: int | None = logging.DEBUG,
 ):
     """Write the output to the log."""
+    if level is None:
+        return
     message = strip_ansi(message.rstrip())
     for line in message.splitlines():
         logger.log(level, f"<[{channel}] {line}")
--- tests/realms/test_subprocess.py
+++ tests/realms/test_subprocess.py
@@ -3,7 +3,7 @@ import subprocess
 
 import pytest
 
-from secrets_env.realms.subprocess import check_output, log_output
+from secrets_env.realms.subprocess import check_output, write_output
 
 
 class TestCheckOutput:
@@ -20,16 +20,32 @@ class TestCheckOutput:
             caplog.at_level(logging.DEBUG),
             pytest.raises(subprocess.CalledProcessError),
         ):
-            check_output(["sh", "-c", "echo Ah! > /dev/stderr && false"])
+            check_output(
+                [
+                    "sh",
+                    "-c",
+                    """
+                    echo Wow!
+                    echo Ah! > /dev/stderr
+                    false
+                    """,
+                ]
+            )
 
         assert "< return code: 1" in caplog.text
+        assert "<[stdout] Wow!" in caplog.text
         assert "<[stderr] Ah!" in caplog.text
 
 
-class TestLogOutput:
-    def test(self, caplog: pytest.LogCaptureFixture):
+class TestWriteOutput:
+    def test_success(self, caplog: pytest.LogCaptureFixture):
         with caplog.at_level(logging.DEBUG):
-            log_output("stdout", "hello world!")
-            log_output("stderr", "\x1b[31mTest error\x1b[39m")
+            write_output("stdout", "hello world!")
+            write_output("stderr", "\x1b[31mTest error\x1b[39m")
         assert "<[stdout] hello world!" in caplog.text
         assert "<[stderr] Test error" in caplog.text
+
+    def test_skip(self, caplog: pytest.LogCaptureFixture):
+        with caplog.at_level(logging.DEBUG):
+            write_output("stdout", "hello world!", level=None)
+        assert caplog.text == ""
