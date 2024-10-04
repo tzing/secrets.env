9fcab2c587333aeab8626e2412cf5eccb3e12470
tzing <tzingshih@gmail.com>
2024-10-04 20:29:38 +0800

    fix syntax

--- tests/providers/vault/conftest.py
+++ tests/providers/vault/conftest.py
@@ -8,7 +8,7 @@ from secrets_env.providers.vault import VaultKvProvider
 @pytest.fixture
 def intl_provider() -> VaultKvProvider:
     if "VAULT_ADDR" not in os.environ:
-        raise pytest.skip("VAULT_ADDR is not set")
+        pytest.skip("VAULT_ADDR is not set")
     if "VAULT_TOKEN" not in os.environ:
-        raise pytest.skip("VAULT_TOKEN is not set")
+        pytest.skip("VAULT_TOKEN is not set")
     return VaultKvProvider(auth="token")
