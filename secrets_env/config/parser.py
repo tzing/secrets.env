c75cecd989bfd4eb2dc0c1f85127fb82dd4d19cc
tzing <tzingshih@gmail.com>
2025-08-31 20:19:08 +0800

    refactor: parser

--- secrets_env/config/parser.py
+++ secrets_env/config/parser.py
@@ -1,33 +1,96 @@
 from __future__ import annotations
 
 import contextlib
-import itertools
-from typing import TYPE_CHECKING, cast
+import typing
 
 from pydantic import BaseModel, Field, ValidationError, field_validator, model_validator
 
-from secrets_env.provider import Provider, Request
+from secrets_env.provider import AsyncProvider, Provider, Request
 from secrets_env.providers import get_provider
 
-if TYPE_CHECKING:
-    from collections.abc import Iterator, Sequence
-    from typing import TypeVar
+if typing.TYPE_CHECKING:
+    from collections.abc import Sequence
+    from typing import Self
 
     from pydantic import ValidationInfo
     from pydantic_core import ErrorDetails
 
-    _T = TypeVar("_T")
+
+class LocalConfig(BaseModel):
+    """Data model that represents a local configuration file."""
+
+    sources: list[Provider | AsyncProvider] = Field(default_factory=list)
+    secrets: list[Request] = Field(default_factory=list)
+
+    @model_validator(mode="before")
+    @classmethod
+    def _before_validator_(cls, values):
+        if isinstance(values, dict):
+            errors = []
+
+            with capture_line_errors(errors, ()):
+                providers = ProviderBuilder.model_validate(values)
+                values["sources"] = providers.source + providers.sources
+            with capture_line_errors(errors, ()):
+                requests = RequestBuilder.model_validate(values)
+                values["secrets"] = requests.secret + requests.secrets
+
+            if errors:
+                raise ValidationError.from_exception_data(
+                    title="local config", line_errors=errors
+                )
+
+        return values
+
+    @model_validator(mode="after")
+    def _check_source_exists_(self) -> Self:
+        # collect available source names
+        available_sources = {provider.name for provider in self.sources}
+        has_default_source = len(self.sources) == 1
+
+        # ensure all requests have a valid source
+        errors = []
+        for request in self.secrets:
+            if request.source in available_sources:
+                continue  # valid
+            if request.source is None and has_default_source:
+                continue  # use default
+
+            if request.source is None:
+                errors.append(
+                    {
+                        "type": "missing",
+                        "loc": ("secrets", request.name, "source"),
+                    }
+                )
+            else:
+                errors.append(
+                    {
+                        "type": "value_error",
+                        "loc": ("secrets", request.name, "source"),
+                        "input": request.source,
+                        "ctx": {"error": f'source "{request.source}" not found'},
+                    }
+                )
+
+        # raise errors
+        if errors:
+            raise ValidationError.from_exception_data(
+                title="local config", line_errors=errors
+            )
+
+        return self
 
 
-class _ProviderBuilder(BaseModel):
+class ProviderBuilder(BaseModel):
     """Internal helper to build provider instances from source(s) configs."""
 
-    source: list[Provider] = Field(default_factory=list)
-    sources: list[Provider] = Field(default_factory=list)
+    source: list[Provider | AsyncProvider] = Field(default_factory=list)
+    sources: list[Provider | AsyncProvider] = Field(default_factory=list)
 
     @field_validator("source", "sources", mode="before")
     @classmethod
-    def _transform(cls, value, info: ValidationInfo):
+    def _transform_(cls, value, info: ValidationInfo):
         if isinstance(value, dict):
             value = [value]
 
@@ -43,59 +106,43 @@ class _ProviderBuilder(BaseModel):
 
             if errors:
                 raise ValidationError.from_exception_data(
-                    title=cast("str", info.field_name), line_errors=errors
+                    title=typing.cast("str", info.field_name), line_errors=errors
                 )
             return providers
 
         raise ValueError("Input must be a list or a dictionary")
 
-    def to_dict(self) -> dict[str, Provider]:
-        """
-        Returns an iterator of tuples with the provider name and the provider instance.
+    @model_validator(mode="after")
+    def _check_dupe_name_(self) -> Self:
+        seen_names = {}
+        for idx, provider in enumerate(self.source):
+            seen_names.setdefault(provider.name, []).append(("source", idx))
+        for idx, provider in enumerate(self.sources):
+            seen_names.setdefault(provider.name, []).append(("sources", idx))
 
-        Raises
-        ------
-        ValidationError
-            If the source names are not unique.
-        """
-        providers = {}
         errors = []
-
-        for provider in itertools.chain(self.source, self.sources):
-            if provider.name in providers:
+        for name, locations in seen_names.items():
+            if len(locations) == 1:
+                continue
+            for loc in locations:
                 errors.append(
                     {
                         "type": "value_error",
-                        "loc": ("sources", "*", "name"),
-                        "input": provider.name,
-                        "ctx": {"error": "duplicated source name"},
+                        "loc": (*loc, "name"),
+                        "input": name,
+                        "ctx": {"error": "duplicated provider name"},
                     }
                 )
-            else:
-                providers[provider.name] = provider
 
         if errors:
             raise ValidationError.from_exception_data(
                 title="sources", line_errors=errors
             )
 
-        return providers
+        return self
 
 
-def validate_providers(values: _T) -> _T:
-    """
-    Build source(s) configs into provider instances.
-    This function is intended to be used as a before validator for Pydantic models
-    with the attribute ``providers`` of type ``dict[str, Provider]``.
-    """
-    if isinstance(values, dict):
-        adapter = _ProviderBuilder.model_validate(values)
-        providers = values.setdefault("providers", {})
-        providers.update(adapter.to_dict())
-    return values
-
-
-class _RequestBuilder(BaseModel):
+class RequestBuilder(BaseModel):
     """Internal helper to build request instances from secret(s) configs."""
 
     secret: list[Request] = Field(default_factory=list)
@@ -103,7 +150,7 @@ class _RequestBuilder(BaseModel):
 
     @field_validator("secret", "secrets", mode="before")
     @classmethod
-    def _transform(cls, value: list | dict[str, dict | str], info: ValidationInfo):
+    def _transform_(cls, value: list | dict[str, dict | str], info: ValidationInfo):
         if isinstance(value, list):
             return value
 
@@ -123,105 +170,38 @@ class _RequestBuilder(BaseModel):
 
             if errors:
                 raise ValidationError.from_exception_data(
-                    title=cast("str", info.field_name), line_errors=errors
+                    title=typing.cast("str", info.field_name), line_errors=errors
                 )
             return requests
 
         # type error here does not get caught by pydantic
         raise ValueError(f'expect list or dict for "{info.field_name}"')
 
-    def iter_requests(self) -> Iterator[Request]:
-        seen_names = set()
-        errors = []
+    @model_validator(mode="after")
+    def _check_dupe_name_(self) -> Self:
+        seen_names = {}
+        for idx, request in enumerate(self.secret):
+            seen_names.setdefault(request.name, []).append(("secret", idx))
+        for idx, request in enumerate(self.secrets):
+            seen_names.setdefault(request.name, []).append(("secrets", idx))
 
-        for request in itertools.chain(self.secret, self.secrets):
-            if request.name in seen_names:
+        errors = []
+        for name, locations in seen_names.items():
+            if len(locations) == 1:
+                continue
+            for loc in locations:
                 errors.append(
                     {
                         "type": "value_error",
-                        "loc": ("secrets", request.name),
-                        "input": request.name,
+                        "loc": (*loc, "name"),
+                        "input": name,
                         "ctx": {"error": "duplicated secret name"},
                     }
                 )
 
-            else:
-                seen_names.add(request.name)
-                yield request
-
         if errors:
             raise ValidationError.from_exception_data(
                 title="secrets", line_errors=errors
-            )
-
-
-def validate_requests(values: _T) -> _T:
-    """
-    Build secret(s) configs into request instances.
-
-    This function is intended to be used as a before validator for Pydantic models
-    with the attribute ``requests`` of type ``list[Request]``.
-    """
-    if isinstance(values, dict):
-        adapter = _RequestBuilder.model_validate(values)
-        requests = values.setdefault("requests", [])
-        requests.extend(adapter.iter_requests())
-    return values
-
-
-class LocalConfig(BaseModel):
-    """Data model that represents a local configuration file."""
-
-    providers: dict[str | None, Provider] = Field(default_factory=dict)
-    requests: list[Request] = Field(default_factory=list)
-
-    @model_validator(mode="before")
-    @classmethod
-    def _before_validator(cls, values):
-        errors = []
-        with capture_line_errors(errors, ()):
-            values = validate_providers(values)
-        with capture_line_errors(errors, ()):
-            values = validate_requests(values)
-        if errors:
-            raise ValidationError.from_exception_data(
-                title="local config", line_errors=errors
-            )
-        return values
-
-    @model_validator(mode="after")
-    def _check_source_exists(self):
-        # if there is only one source, it will be used as the default source
-        default_source = None
-        if len(self.providers) == 1:
-            default_source = next(iter(self.providers))
-
-        # check if all requests have a valid source
-        errors = []
-        for request in self.requests:
-            applied_source = request.source or default_source
-            if applied_source not in self.providers:
-                if request.source is None:
-                    errors.append(
-                        {
-                            "type": "missing",
-                            "loc": ("secrets", request.name, "source"),
-                        }
-                    )
-                else:
-                    errors.append(
-                        {
-                            "type": "value_error",
-                            "loc": ("secrets", request.name, "source"),
-                            "input": request.source,
-                            "ctx": {"error": f'source "{request.source}" not found'},
-                        }
-                    )
-
-        # raise errors
-        if errors:
-            raise ValidationError.from_exception_data(
-                title="local config", line_errors=errors
             )
 
         return self
