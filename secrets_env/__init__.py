4a262f1f22c408a5de7a9ac20e80943a25a8d277
tzing <tzingshih@gmail.com>
2025-09-02 00:21:00 +0800

    add: async load values

--- secrets_env/__init__.py
+++ secrets_env/__init__.py
@@ -1,15 +1,20 @@
 from __future__ import annotations
 
+import asyncio
 import logging
 import typing
 
 import secrets_env.config
 import secrets_env.exceptions
 import secrets_env.utils
+from secrets_env.provider import AsyncProvider, Provider, Request
 from secrets_env.version import __version__  # noqa: F401
 
 if typing.TYPE_CHECKING:
+    from asyncio import Task
     from pathlib import Path
+
+logger = logging.getLogger(__name__)
 
 
 def read_values(*, config: Path | None, strict: bool) -> dict[str, str]:
@@ -36,8 +41,6 @@ def read_values(*, config: Path | None, strict: bool) -> dict[str, str]:
     NoValue
         When failed to load a value and strict mode is enabled.
     """
-    logger = logging.getLogger(__name__)
-
     if secrets_env.utils.get_bool_from_env_var("SECRETS_ENV_DISABLE"):
         logger.warning(
             "The environment variable 'SECRETS_ENV_DISABLE' is configured. "
@@ -73,13 +76,13 @@ def read_values(*, config: Path | None, strict: bool) -> dict[str, str]:
     # report
     if is_success:
         logger.info(
-            "<!important>\U0001F511 <mark>%d</mark> secrets loaded", len(cfg.requests)
+            "<!important>\U0001f511 <mark>%d</mark> secrets loaded", len(cfg.requests)
         )
 
     elif strict:
         logger.error(
             # NOTE need extra whitespace after the modifier (\uFE0F)
-            "<!important>\u26A0\uFE0F  <error>%d</error> / %d secrets loaded. "
+            "<!important>\u26a0\ufe0f  <error>%d</error> / %d secrets loaded. "
             "Not satisfied the requirement.",
             len(output_values),
             len(cfg.requests),
@@ -89,7 +92,115 @@ def read_values(*, config: Path | None, strict: bool) -> dict[str, str]:
     else:
         logger.warning(
             # NOTE need extra whitespace after the modifier (\uFE0F)
-            "<!important>\u26A0\uFE0F  <error>%d</error> / %d secrets loaded",
+            "<!important>\u26a0\ufe0f  <error>%d</error> / %d secrets loaded",
+            len(output_values),
+            len(cfg.requests),
+        )
+
+    return output_values
+
+
+async def load_values(*, config: Path | None, strict: bool) -> dict[str, str]:
+    """
+    Load values from the providers and return them as a dictionary.
+
+    Parameters
+    ----------
+    config : Path | None
+        Path to config file. It searchs for config file when not given.
+    strict : bool
+        Enable strict mode. Raises an error when not all of the requests are
+        successfully loaded.
+
+    Returns
+    -------
+    dict[str, str]
+        A dictionary of the loaded values.
+
+    Raises
+    ------
+    ConfigError
+        When the configuration is malformed.
+    NoValue
+        When failed to load a value and strict mode is enabled.
+    """
+    if secrets_env.utils.get_bool_from_env_var("SECRETS_ENV_DISABLE"):
+        logger.warning(
+            "The environment variable 'SECRETS_ENV_DISABLE' is configured. "
+            "The value loading process will be bypassed."
+        )
+        return {}
+
+    # parse config
+    cfg = secrets_env.config.load_local_config(config)
+
+    if not cfg.secrets:
+        logger.info("Requests are absent. Skipping values loading.")
+        return {}
+
+    # index providers
+    providers: dict[str | None, Provider | AsyncProvider] = {
+        provider.name: provider for provider in cfg.sources
+    }
+
+    if len(cfg.sources) == 1:
+        providers[None] = cfg.sources[0]
+
+    # submit requests
+    lock = asyncio.Lock()
+
+    async def _fetch(request: Request) -> tuple[Request, str]:
+        nonlocal providers, lock
+
+        provider = providers[request.source]
+
+        if isinstance(provider, AsyncProvider):
+            value = await provider(request)
+        else:
+            # TODO remove lock after the sync provider is thread-safe
+            async with lock:
+                value = provider(request)
+
+        return request, value
+
+    tasks: list[Task[tuple[Request, str]]] = []
+    for request in cfg.requests:
+        tasks.append(asyncio.create_task(_fetch(request)))
+
+    # collect results
+    output_values = {}
+    is_success = True
+
+    for task in asyncio.as_completed(tasks):
+        try:
+            request, value = await task
+        except secrets_env.exceptions.NoValue:
+            is_success = False
+            continue
+
+        logger.debug(f"Loaded <data>{request.name}</data>")
+        output_values[request.name] = value
+
+    # report
+    if is_success:
+        logger.info(
+            "<!important>\U0001f511 <mark>%d</mark> secrets loaded", len(cfg.requests)
+        )
+
+    elif strict:
+        logger.error(
+            # NOTE need extra whitespace after the modifier (\uFE0F)
+            "<!important>\u26a0\ufe0f  <error>%d</error> / %d secrets loaded. "
+            "Not satisfied the requirement.",
+            len(output_values),
+            len(cfg.requests),
+        )
+        raise secrets_env.exceptions.NoValue
+
+    else:
+        logger.warning(
+            # NOTE need extra whitespace after the modifier (\uFE0F)
+            "<!important>\u26a0\ufe0f  <error>%d</error> / %d secrets loaded",
             len(output_values),
             len(cfg.requests),
         )
--- tests/test_secrets_env.py
+++ tests/test_secrets_env.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from secrets_env import read_values
+from secrets_env import load_values, read_values
 from secrets_env.exceptions import ConfigError, NoValue
 
 
@@ -142,6 +142,149 @@ class TestReadValues:
         monkeypatch.setenv("SECRETS_ENV_DISABLE", "1")
 
         values = read_values(config=config_file, strict=True)
+        assert values == {}
+
+        assert "The value loading process will be bypassed" in caplog.text
+
+
+class TestLoadValues:
+
+    @pytest.mark.asyncio
+    async def test_plain_text(self, tmp_path: Path, caplog: pytest.LogCaptureFixture):
+        config_file = tmp_path / "config.yaml"
+        config_file.write_text(
+            """
+            sources:
+              type: plain
+
+            secrets:
+              DEMO: Hello, World!
+            """
+        )
+
+        with caplog.at_level("INFO"):
+            values = await load_values(config=config_file, strict=True)
+
+        assert values == {"DEMO": "Hello, World!"}
+        assert "<mark>1</mark> secrets loaded" in caplog.text
+
+    @pytest.mark.asyncio
+    async def test_multiple_sources(
+        self, tmp_path: Path, caplog: pytest.LogCaptureFixture
+    ):
+        config_file = tmp_path / "config.yaml"
+        config_file.write_text(
+            """
+            sources:
+              - name: demo-1
+                type: debug:sync
+                value: FooBar
+              - name: demo-2
+                type: debug:async
+                value: BazQax
+
+            secrets:
+              - name: DEMO_1
+                source: demo-1
+              - name: DEMO_2
+                source: demo-2
+            """
+        )
+
+        with caplog.at_level("DEBUG"):
+            values = await load_values(config=config_file, strict=True)
+
+        assert values == {"DEMO_1": "FooBar", "DEMO_2": "BazQax"}
+        assert "Loaded <data>DEMO_1</data>" in caplog.text
+        assert "Loaded <data>DEMO_2</data>" in caplog.text
+        assert "<mark>2</mark> secrets loaded" in caplog.text
+
+    @pytest.mark.asyncio
+    async def test_empty(self, tmp_path: Path, caplog: pytest.LogCaptureFixture):
+        config_file = tmp_path / "config.toml"
+        config_file.write_text(
+            """
+            [[sources]]
+            type = "debug"
+            value = "foobar"
+            """
+        )
+
+        with caplog.at_level("DEBUG"):
+            values = await load_values(config=config_file, strict=True)
+
+        assert values == {}
+        assert "Requests are absent." in caplog.text
+
+    @pytest.mark.asyncio
+    async def test_no_value__strict(
+        self, tmp_path: Path, caplog: pytest.LogCaptureFixture
+    ):
+        config_file = tmp_path / "config.toml"
+        config_file.write_text(
+            """
+            [[sources]]
+            type = "plain"
+
+            [[secrets]]
+            name = "DEMO"
+            """
+        )
+
+        with pytest.raises(NoValue):
+            await load_values(config=config_file, strict=True)
+
+        assert "Value for <data>DEMO</data> not found" in caplog.text
+        assert "<error>0</error> / 1 secrets loaded" in caplog.text
+
+    @pytest.mark.asyncio
+    async def test_no_value__tolerated(
+        self, tmp_path: Path, caplog: pytest.LogCaptureFixture
+    ):
+        config_file = tmp_path / "config.toml"
+        config_file.write_text(
+            """
+            [[sources]]
+            type = "plain"
+
+            [[secrets]]
+            name = "DEMO"
+
+            [[secrets]]
+            name = "FOO"
+            value = "Bar"
+            """
+        )
+
+        values = await load_values(config=config_file, strict=False)
+        assert values == {"FOO": "Bar"}
+
+        assert "Value for <data>DEMO</data> not found" in caplog.text
+        assert "<error>1</error> / 2 secrets loaded" in caplog.text
+
+    @pytest.mark.asyncio
+    async def test_config_error(self, tmp_path: Path):
+        config_file = tmp_path / "config.toml"
+        config_file.write_text(
+            """
+            [[sources]]
+            type = "non-existent-source"
+            """
+        )
+
+        with pytest.raises(ConfigError):
+            await load_values(config=config_file, strict=True)
+
+    @pytest.mark.asyncio
+    async def test_disable(
+        self,
+        monkeypatch: pytest.MonkeyPatch,
+        tmp_path: Path,
+        caplog: pytest.LogCaptureFixture,
+    ):
+        monkeypatch.setenv("SECRETS_ENV_DISABLE", "1")
+
+        values = await load_values(config=tmp_path / "config.toml", strict=True)
         assert values == {}
 
         assert "The value loading process will be bypassed" in caplog.text
