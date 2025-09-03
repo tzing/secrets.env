cbf9eb0450fa6c25b7ed5e352360f9dddb917bf2
tzing <tzingshih@gmail.com>
2025-09-03 20:44:13 +0800

    migrate: debug, plain provider

--- secrets_env/providers/plain.py
+++ secrets_env/providers/plain.py
@@ -2,19 +2,19 @@ from __future__ import annotations
 
 import typing
 
-from secrets_env.provider import Provider
+from secrets_env.provider import AsyncProvider
 
 if typing.TYPE_CHECKING:
     from secrets_env.provider import Request
 
 
-class PlainTextProvider(Provider):
+class PlainTextProvider(AsyncProvider):
     """Plain text provider returns text that is copied directly from the
     configuration file."""
 
     type = "plain"
 
-    def _get_value_(self, spec: Request) -> str:
+    async def _get_value_(self, spec: Request) -> str:
         if spec.value is None:
             raise LookupError("Value not provided in the configuration")
         return spec.value
